a86f1e8138ee4ae2b98d33c5dc75ae8fd21c043c
Tomomi Hobara <tomomihobra@hotmail.com>
2025-02-04 13:36:35 -0600

    Upload two files

---
+++ script.js
@@ -0,0 +1,13 @@
+// Event lister that will trigger when DOM is loaded (= when visiting the page)
+addEventListener('DOMContentLoaded', async function () {
+    const response = await this.fetch("http://localhost:3000/api/songs");
+
+    const songs = await response.json();
+
+    let html = '';
+    for (let song of songs) {
+        html += `<li>${song.title} - ${song.artist}</li>`;
+    } 
+    
+    document.querySelector("#addedsong").innerHTML = html;  // Insert the html to the div with the id "addedsong"
+});
