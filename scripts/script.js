1e1eda2b7b66753e9fe9becf6ad7280c1dbc7ecc
Tomomi Hobara <tomomihobra@hotmail.com>
2025-02-14 17:17:59 -0600

    Added user to index

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ addEventListener('DOMContentLoaded', async function () {
     let html = '';
     for (let song of songs) {
         let songID = song._id;
-        html += `<li>${song.title} - ${song.artist} - <a href="details.html?id=${songID}">Details</a> - <a href="edit.html?id=${songID}">Edit Song</a></li>`;  // Add the song title and artist to the html
+        html += `<li>${song.title} - ${song.artist} - ${song.username} - <a href="details.html?id=${songID}">Details</a> - <a href="edit.html?id=${songID}">Edit Song</a></li>`;  // Add the song title and artist to the html
     } 
     
     document.querySelector("#list_of_songs").innerHTML = html;  // Insert the html to the div with the id "addedsong"
